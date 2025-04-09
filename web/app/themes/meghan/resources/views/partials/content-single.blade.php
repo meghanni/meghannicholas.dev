2b4acd7974921953b361d4e0998ea2577f020461
Meghan Nicholas <meghan@megnicholas.com>
2025-04-09 16:22:10 +0100

    updated

--- web/app/themes/meghan/resources/views/partials/content-single.blade.php
+++ web/app/themes/meghan/resources/views/partials/content-single.blade.php
@@ -1,23 +1,23 @@
 <article @php(post_class('h-entry'))>
-  <header>
-    <h1 class="p-name">
-      {!! $title !!}
-    </h1>
+    <header>
+        <h1 class="p-name text-primary-200">
+            {!! $title !!}
+        </h1>
 
-    @include('partials.entry-meta')
-  </header>
+        @include('partials.entry-meta')
+    </header>
 
-  <div class="e-content">
-    @php(the_content())
-  </div>
+    <div class="e-content">
+        @php(the_content())
+    </div>
 
-  @if ($pagination())
-    <footer>
-      <nav class="page-nav" aria-label="Page">
-        {!! $pagination !!}
-      </nav>
-    </footer>
-  @endif
+    @if ($pagination())
+        <footer>
+            <nav class="page-nav" aria-label="Page">
+                {!! $pagination !!}
+            </nav>
+        </footer>
+    @endif
 
-  @php(comments_template())
+    @php(comments_template())
 </article>
--- web/app/themes/meghan/resources/views/sections/header.blade.php
+++ web/app/themes/meghan/resources/views/sections/header.blade.php
@@ -9,3 +9,5 @@
     </nav>
   @endif
 </header>
+
+
